63d55f236ffa850e69b81f44016b18b87b43c319
oscarsotelo <oscaresotelo@gmail.com>
2024-05-03 13:00:03 -0300

    oc

--- nuevoplayer.py
+++ nuevoplayer.py
@@ -1,27 +1,23 @@
+import base64
+
 import streamlit as st
-import os
 
-# Carpeta donde se encuentran los archivos MP3
-mp3_folder = 'downloads'
-mp3_files = [f for f in os.listdir(mp3_folder) if f.endswith('.mp3')]
 
-# Lista de archivos MP3
-st.header("Lista de archivos MP3:")
-selected_file = st.selectbox("Seleccionar archivo MP3:", mp3_files)
+def autoplay_audio(file_path: str):
+    with open(file_path, "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        md = f"""
+            <audio controls autoplay="true">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            </audio>
+            """
+        st.markdown(
+            md,
+            unsafe_allow_html=True,
+        )
 
-# Obtener la URL del archivo seleccionado
-if selected_file:
-    audio_path = os.path.join(mp3_folder, selected_file)
-    audio_url = f"/{audio_path}"
 
-    # Cargar código JavaScript para controlar la reproducción automática del audio
-    js_code = f"""
-    <script>
-    var audioElement = new Audio("{audio_url}");
-    audioElement.autoplay = true;
-    audioElement.load();
-    </script>
-    """
+st.write("# Auto-playing Audio!")
 
-    # Mostrar el reproductor de audio
-    st.markdown(js_code, unsafe_allow_html=True)
+autoplay_audio("1.mp3")
